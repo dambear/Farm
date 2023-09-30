4ad631162918f89c12bee05b594f2196d3f19573
Jayvenarellano <jayvenarellano2002@gmail.com>
2023-09-30 19:04:06 +0800

    New updated table and icons

--- src/components/Farmer/UpdateFarmerModal.jsx
+++ src/components/Farmer/UpdateFarmerModal.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BsFillTelephonePlusFill, BsFillCalendarEventFill,  BsFillClipboardPlusFill } from "react-icons/bs";
 import { updateDoc } from 'firebase/firestore';
 
 /*UPDATE*/
@@ -29,33 +30,37 @@ function UpdateFarmerModal({ farmer, editedData, setEditedData, handleSave, hand
     };
 
     return (
-        <div className="fixed inset-0 flex items-center justify-center z-50 ">
-            <div className="bg-white p-6 rounded-lg w-96">
-                <h2 className="text-xl font-semibold mb-4">Edit Farmer</h2>
+        <div className="fixed inset-0 flex items-center  justify-center z-50 ">
+            <div className="bg-white shadow-lg p-6 border-2 rounded-lg w-100">
+                <h2 className="text-xl font-semibold mb-4 flex items-center"><BsFillClipboardPlusFill size='24' className='fill-green-500 mr-2'/>Edit Farmer</h2>
                 <form onSubmit={handleModalSubmit} >
-                    <div className="mb-4">
+
+                    <div className="grid grid-cols-2 gap-5">
+
+                    <div className="mt-4">
                         <label className="block mb-2">First Name</label>
                         <input
+                            id="firstname"
                             type="text"
-                            className="border rounded-md w-full p-2"
+                            className="border rounded-md w-full p-2 focus:outline-none focus:border-emerald-600"
                             value={editedData.first_name}
                             onChange={(e) => setEditedData({ ...editedData, first_name: e.target.value })}
                         />
                     </div>
-                    <div className="mb-4">
+                    <div className="mt-4">
                         <label className="block mb-2">Last Name</label>
                         <input
                             type="text"
-                            className="border rounded-md w-full p-2"
+                            className="border rounded-md w-full p-2 focus:outline-none focus:border-emerald-600"
                             value={editedData.last_name}
                             onChange={(e) => setEditedData({ ...editedData, last_name: e.target.value })}
                         />
                     </div>
-                    <div className="mb-4">
-                        <label className="block mb-2">Age</label>
+                    <div className="mt-4">
+                        <label className="block mb-2 flex items-center"><BsFillCalendarEventFill size='18' className='fill-green-500 mr-2'/>Age</label>
                         <input
                             type="text"
-                            className="border rounded-md w-full p-2"
+                            className="border rounded-md w-full p-2 focus:outline-none focus:border-emerald-600"
                             value={editedData.age}
                             onChange={(e) => {
                                 // Ensure only numeric input and limit to 2 characters
@@ -64,11 +69,11 @@ function UpdateFarmerModal({ farmer, editedData, setEditedData, handleSave, hand
                             }}
                         />
                     </div>
-                    <div className="mb-4">
-                        <label className="block mb-2">Contact Number</label>
+                    <div className="mt-4">
+                        <label className="block mb-2 flex items-center"><BsFillTelephonePlusFill size='18' className='fill-green-500 mr-2'/>Contact Number</label>
                         <input
                             type="text"
-                            className="border rounded-md w-full p-2"
+                            className="border rounded-md w-full p-2 focus:outline-none focus:border-emerald-600"
                             value={editedData.contact_number}
                             onChange={(e) => {
                                 // Ensure only numeric input and limit to 11 characters
@@ -77,11 +82,14 @@ function UpdateFarmerModal({ farmer, editedData, setEditedData, handleSave, hand
                             }}
                         />
                     </div>
-                    <div className="flex justify-end">
-                        <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md mr-2">
+
+                    </div>
+                    
+                    <div className="flex justify-end mt-4">
+                        <button type="submit" className="bg-emerald-400 hover:bg-emerald-700 text-white px-4 py-2 rounded-md mr-2">
                             Save
                         </button>
-                        <button className="bg-gray-300 px-4 py-2 rounded-md" onClick={handleCloseModal}>
+                        <button className="bg-gray-300 hover:bg-gray-500 px-4 py-2 rounded-md" onClick={handleCloseModal}>
                             Cancel
                         </button>
                     </div>
--- src/pages/SoilModule.jsx
+++ src/pages/SoilModule.jsx
@@ -3,198 +3,234 @@ import React from 'react'
 function SoilModule() {
   return (
     <div className="bg-gray-300 absolute p-5 right-0 w-5/6 h-screen">
-      <h1 className='text-xl font-semibold mb-2'>Soil Module</h1>
+      {/*
+        <h1 className='text-2xl font-bold mb-4 '>Soil Module</h1>
+      */}
 
           <div className='overflow-auto rounded-lg shadow'>
-                <table className='w-full'>
-                <thead className='bg-gray-50 border-b-2 gray-200'>
+
+                <table className='min-w-full bg-white mb-10'>
+                <thead className='bg-emerald-400 shadow-md'>
                     <tr>
-                        <th className='p-3 text-sm font-semi-bold tracking-wide text-left'>Nutrient</th>
-                        <th className='p-3 text-sm font-semi-bold tracking-wide text-left'>Optimal Range</th>
-                        <th className='p-3 text-sm font-semi-bold tracking-wide text-left'>Unit</th>
-                        <th className='p-3 text-sm font-semi-bold tracking-wide text-left'>Function</th>
+                        <th className='p-3 text-white text-lg font-semi-bold tracking-wide items-center'>Nutrient</th>
+                        <th className='p-3 text-white text-lg font-semi-bold tracking-wide items-center'>Optimal Range</th>
+                        <th className='p-3 text-white text-lg font-semi-bold tracking-wide items-center'>Unit</th>
+                        <th className='p-3 text-white text-lg font-semi-bold tracking-wide items-center'>Function</th>
 
                     </tr>
                 </thead>
                 <tbody className='divide-y divide-gray-100'>
 
                     {/*ROW 1*/}
-                    <tr className='bg-white'>
-                        <td className='p-3 text-sm text-gray-700'>
+                    <tr >
+                        <td className='p-1 text-sl font-medium text-gray-700 text-center border text-black'>
+                          <span className='p-1.5 text-sl font-medium uppercase tracking-wider text-red-600 bg-red-200 rounded-lg'>
                           Nitrogen (N)
+                          </span>
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-xl font-medium  font-bold text-blue-500 text-center border'>
                           0.05 - 0.15%
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-xl font-medium text-gray-700 text-center border text-black'>
                           %
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-sl font-medium text-gray-700 text-center border text-black'>
                           Promotes leafy growth and overall vigor
                         </td>
                     </tr>
+                    
                     {/*ROW 2*/}
-                    <tr className='bg-gray-400'>
-                        <td className='p-3 text-sm text-gray-700'>
+                    <tr>
+                        <td className='p-1 text-sl font-medium text-gray-700 text-center border text-black'>
+                          <span className='p-1.5 text-sl font-medium uppercase tracking-wider text-red-600 bg-red-200 rounded-lg'>
                           Phosphorus (P)
+                          </span>
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-xl font-medium  font-bold text-blue-500 text-center border'>
                           10 - 40 ppm
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-xl font-medium text-gray-700 text-center border text-black'>
                           parts per million (ppm)
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-sl font-medium text-gray-700 text-center border text-black'>
                           Supports root development and flowering
                         </td>
                     </tr>
+
                     {/*ROW 3*/}
-                    <tr className='bg-white'>
-                        <td className='p-3 text-sm text-gray-700'>
-                          Potassium (K)
+                    <tr>
+                        <td className='p-1 text-sl font-medium text-gray-700 text-center border text-black'>
+                          <span className='p-1.5 text-sl font-medium uppercase tracking-wider text-amber-600 bg-amber-200 rounded-lg'>
+                            Potassium (K)
+                          </span>
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-xl font-medium  font-bold text-blue-500 text-center border'>
                           100 - 300 ppm
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-xl font-medium text-gray-700 text-center border text-black'>
                           ppm
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-sl font-medium text-gray-700 text-center border text-black'>
                           Enhance disease resistance and fruiting
                         </td>
                     </tr>
+
                     {/*ROW 4*/}
-                    <tr className='bg-gray-400'>
-                        <td className='p-3 text-sm text-gray-700'>
+                    <tr>
+                        <td className='p-1 text-sl font-medium text-gray-700 text-center border text-black'>
+                          <span className='p-1.5 text-sl font-medium uppercase tracking-wider text-orange-600 bg-orange-200 rounded-lg'>
                           Calcium (Ca)
+                          </span>
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-xl font-medium  font-bold text-blue-500 text-center border'>
                           1.5 - 4.0%
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-xl font-medium text-gray-700 text-center border text-black'>
                           %
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-sl font-medium text-gray-700 text-center border text-black'>
                           Aids in cell wall structure and nutrient uptake
                         </td>
                     </tr>
+
                     {/*ROW 5*/}
-                    <tr className='bg-white'>
-                        <td className='p-3 text-sm text-gray-700'>
-                          Magnesium (Mg)
+                    <tr>
+                        <td className='p-1 text-sl font-medium text-gray-700 text-center border text-black'>
+                          <span className='p-1.5 text-sl font-medium uppercase tracking-wider text-orange-600 bg-orange-200 rounded-lg'>
+                            Magnesium (Mg)
+                          </span>
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-xl font-medium  font-bold text-blue-500 text-center border'>
                           0.2 - 0.5%
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-xl font-medium text-gray-700 text-center border text-black'>
                           %
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-sl font-medium text-gray-700 text-center border text-black'>
                           Essential for photosynthesis and enzyme activation
                         </td>
                     </tr>
+
                     {/*ROW 6*/}
-                    <tr className='bg-gray-400'>
-                        <td className='p-3 text-sm text-gray-700'>
-                          Sulfur (S)
+                    <tr>
+                        <td className='p-1 text-sl font-medium text-gray-700 text-center border text-black'>
+                          <span className='p-1.5 text-sl font-medium uppercase tracking-wider text-red-600 bg-red-200 rounded-lg'>
+                            Sulfur (S)
+                          </span>
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-xl font-medium  font-bold text-blue-500 text-center border'>
                           0.2 - 0.5%
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-xl font-medium text-gray-700 text-center border text-black'>
                           %
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-sl font-medium text-gray-700 text-center border text-black'>
                           Important for amino acid and protein formation
                         </td>
                     </tr>
+
                     {/*ROW 7*/}
-                    <tr className='bg-white'>
-                        <td className='p-3 text-sm text-gray-700'>
-                          Iron (Fe)
+                    <tr>
+                        <td className='p-1 text-sl font-medium text-gray-700 text-center border text-black'>
+                          <span className='p-1.5 text-sl font-medium uppercase tracking-wider text-blue-600 bg-blue-200 rounded-lg'>
+                            Iron (Fe)
+                          </span>
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-xl font-medium  font-bold text-blue-500 text-center border'>
                           2 - 5 ppm
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-xl font-medium text-gray-700 text-center border text-black'>
                           ppm
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-sl font-medium text-gray-700 text-center border text-black'>
                           Essential for chlorophyll production
                         </td>
                     </tr>
+
                     {/*ROW 8*/}
-                    <tr className='bg-gray-400'>
-                        <td className='p-3 text-sm text-gray-700'>
-                          Manganese (Mn)
+                    <tr>
+                        <td className='p-1 text-sl font-medium text-gray-700 text-center border text-black'>
+                          <span className='p-1.5 text-sl font-medium uppercase tracking-wider text-blue-600 bg-blue-200 rounded-lg'>
+                            Manganese (Mn)
+                          </span> 
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-xl font-medium  font-bold text-blue-500 text-center border'>
                           1 - 10 ppm
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-xl font-medium text-gray-700 text-center border text-black'>
                           ppm
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
-                          involved in photosynthesis and enzyme activity
+                        <td className='p-3 text-sl font-medium text-gray-700 text-center border text-black'>
+                          Involved in photosynthesis and enzyme activity
                         </td>
                     </tr>
+
                     {/*ROW 9*/}
-                    <tr className='bg-white'>
-                        <td className='p-3 text-sm text-gray-700'>
-                          Zinc (Zn)
+                    <tr >
+                        <td className='p-1 text-sl font-medium text-gray-700 text-center border text-black'>
+                          <span className='p-1.5 text-sl font-medium uppercase tracking-wider text-blue-600 bg-blue-200 rounded-lg'>
+                            Zinc (Zn)
+                          </span> 
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-xl font-medium  font-bold text-blue-500 text-center border'>
                           0.5 - 2 ppm
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-xl font-medium text-gray-700 text-center border text-black'>
                           ppm
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-sl font-medium text-gray-700 text-center border text-black'>
                           Essential for hormone regulation and growth
                         </td>
                     </tr>
+
                     {/*ROW 10*/}
-                    <tr className='bg-gray-400'>
-                        <td className='p-3 text-sm text-gray-700'>
-                          Copper (Cu)
+                    <tr>
+                        <td className='p-1 text-sl font-medium text-gray-700 text-center border text-black'>
+                          <span className='p-1.5 text-sl font-medium uppercase tracking-wider text-blue-600 bg-blue-200 rounded-lg'>
+                            Copper (Cu)
+                          </span>
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-xl font-medium  font-bold text-blue-500 text-center border'>
                           0.2 - 2 ppm
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-xl font-medium text-gray-700 text-center border text-black'>
                           ppm
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
-                          affects nutrient uptake and enzyme function
+                        <td className='p-3 text-sl font-medium text-gray-700 text-center border text-black'>
+                          Affects nutrient uptake and enzyme function
                         </td>
                     </tr>
                     {/*ROW 11*/}
-                    <tr className='bg-white'>
-                        <td className='p-3 text-sm text-gray-700'>
+                    <tr>
+                        <td className='p-1 text-sl font-medium text-gray-700 text-center border text-black'>
+                          <span className='p-1.5 text-sl font-medium uppercase tracking-wider text-gray-600 bg-gray-300 rounded-lg'>
                           Boron (B)
+                          </span>
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-xl font-medium  font-bold text-blue-500 text-center border'>
                           0.5 - 2 ppm
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-xl font-medium text-gray-700 text-center border text-black'>
                           ppm
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-sl font-medium text-gray-700 text-center border text-black'>
                           Important for flower and fruit development
                         </td>
                     </tr>
                     {/*ROW 12*/}
-                    <tr className='bg-gray-400'>
-                        <td className='p-3 text-sm text-gray-700'>
-                          pH (Acidity/Alkalinity)
+                    <tr>
+                        <td className='p-1 text-sl font-medium text-gray-700 text-center border text-black'>
+                          <span className='p-1.5 text-sl font-medium uppercase tracking-wider text-emerald-600 bg-emerald-300 rounded-lg'>
+                            pH (Acidity/Alkalinity)
+                          </span>
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-xl font-medium  font-bold text-blue-500 text-center border'>
                           6.0 - 6.8
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-xl font-medium text-gray-700 text-center border text-black'>
                           -
                         </td>
-                        <td className='p-3 text-sm text-gray-700 whitespace-nowrap'>
+                        <td className='p-3 text-sl font-medium text-gray-700 text-center border text-black'>
                           Influence nutrient availability and microbial activity
                         </td>
                     </tr>
