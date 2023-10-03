bce6e482fad04ea74c79965212119f9f70231f68
dambear <denverasi01@gmail.com>
2023-10-04 00:22:27 +0800

    complete dashboard

--- src/pages/Alert.jsx
+++ src/pages/Alert.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import SecondPanel from '../components/Alert/Secondpanel'
+import SemaphoreSMS from '../components/Alert/SemaphoreSMS'
 
 function Alert() {
   return (
     <div className="bg-gray-300 absolute right-0 w-5/6 h-screen">
-      <h1>This is about page.</h1>
+      <SecondPanel />
+      <SemaphoreSMS />
     </div>
   )
 }
